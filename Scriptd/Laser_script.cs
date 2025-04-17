4bad750af08d7328c49c2d54ddd5fdc92c8a0e14
Kenosos312 <rouvenbryner7@gmail.com>
2025-04-17 16:32:18 +0200

    t

--- Scriptd/Laser_script.cs
+++ Scriptd/Laser_script.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
@@ -8,10 +9,27 @@ public class LaserBeam : MonoBehaviour
     [SerializeField] private LayerMask collisionMask;
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] private GameObject ParticleObject;
+    [SerializeField] private Transform LineStart1;
+    [SerializeField] private Transform LineEnd2;
+    [SerializeField] private float SetTimeforDistance;
+    private float TimeforDistance;
+    private float travelSpeed;
+    [SerializeReference] private Rigidbody2D rb;
+    
+    
+    private void Start()
+    {
+        
+        transform.localPosition = new Vector3(LineStart1.localPosition.x,transform.localPosition.y,transform.localPosition.z);   
+        travelSpeed = (LineStart1.localPosition.x * -1 + LineEnd2.localPosition.x) / SetTimeforDistance;
+        TimeforDistance = SetTimeforDistance;
+    }
+
 
     private void Update()
     {
         CastLaser();
+        MoveLaserMaker();
     }
 
     private void CastLaser()
@@ -31,5 +49,16 @@ public class LaserBeam : MonoBehaviour
         lineRenderer.SetPosition(0, laserOrigin);
         lineRenderer.SetPosition(1, endPosition);
         ParticleObject.transform.position = endPosition;
+    }
+
+    private void MoveLaserMaker(){
+        TimeforDistance -= Time.deltaTime;
+        
+        rb.linearVelocityX = travelSpeed;
+        if(TimeforDistance <= 0){
+            TimeforDistance = SetTimeforDistance;
+            travelSpeed *= -1;
+            
+        }
     }
 }
