f8c5503106ab86f30e5a4a142fb04447db6058ed
Kenosos312 <rouvenbryner7@gmail.com>
2025-04-25 16:58:08 +0200

    p

--- Scriptd/ButtonScript.cs
+++ Scriptd/ButtonScript.cs
@@ -7,15 +7,28 @@ public class ButtonScript : MonoBehaviour
     [SerializeField] SpriteRenderer SpRend;
     float ButtonResetTimer;
 
-
+    [SerializeField] private Transform StartPPlatform1;
+    [SerializeField] private Transform StartPPlatform2;
+    [SerializeField] private Transform EndPPlatform1;
+    [SerializeField] private Transform EndPPlatform2;
+    [SerializeField] private float SetTimeforDistance;
     private float TimeforDistance;
-    private float travelSpeed;
+    private float travelSpeedPlatform1;
+    private float travelSpeedPlatform2;
     [SerializeReference] private GameObject Platform1;
     [SerializeReference] private GameObject Platform2;
+    private Rigidbody2D Rigidbody1;
+    private Rigidbody2D Rigidbody2;
+
+    private void Start() {
+        Rigidbody1 = Platform1.GetComponent<Rigidbody2D>();
+        Rigidbody1 = Platform1.GetComponent<Rigidbody2D>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if(collision.CompareTag("Player")) {
             SpRend.sprite = Button_Active;
+            if(TimeforDistance < 0) CalculatingPlatformSpeed();
         }
     }
     private void OnTriggerStay2D(Collider2D collision) {
@@ -32,5 +45,13 @@ public class ButtonScript : MonoBehaviour
     private void Update() {
         ButtonResetTimer -= Time.deltaTime;
         if(ButtonResetTimer > -0.01 && ButtonResetTimer < 0.01) SpRend.sprite = Button_Raw;
+    }
+
+    private void CalculatingPlatformSpeed() {
+
+        MovingPlatforms();
+    }
+    private void MovingPlatforms() {
+    
     }
 }
